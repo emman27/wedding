a2d47d9631b25fcfb7399068685c8e272e99d705
Emmanuel Goh <emmanuel.goh.7@gmail.com>
2019-03-08 21:21:16 +0800

    Add telegram chatbot
    Signed-off-by: Emmanuel Goh <emmanuel.goh.7@gmail.com>

--- cmd/reporter/main.go
+++ cmd/reporter/main.go
@@ -4,7 +4,10 @@ import (
 	"flag"
 	"os"
 
+	"github.com/emman27/wedding/pkg/notifications"
+
 	"github.com/emman27/wedding/pkg/notifications/stdout"
+	"github.com/emman27/wedding/pkg/notifications/telegram"
 	"github.com/emman27/wedding/pkg/rsvp"
 	"github.com/emman27/wedding/pkg/rsvp/typeform"
 	"github.com/sirupsen/logrus"
@@ -12,7 +15,10 @@ import (
 
 var typeformAPIKey = os.Getenv("TYPEFORM_API_KEY")
 
-var logLevel = flag.String("log-level", "info", "The level to log at. Can be one of debug, info, warn or error")
+var (
+	logLevel         = flag.String("log-level", "info", "The level to log at. Can be one of debug, info, warn or error")
+	notificationType = flag.String("notification-type", "console", "The notification type to get, can choose between console or telegram")
+)
 
 func main() {
 	flag.Parse()
@@ -24,8 +30,25 @@ func main() {
 	logger.SetFormatter(&logrus.TextFormatter{FullTimestamp: true})
 	logger.SetLevel(logLevel)
 	var counter rsvp.Counter = typeform.NewCounter(typeformAPIKey, logger.WithField("component", "typeform.Counter"))
-	var app = rsvp.NewApp(counter, stdout.NewSender(), rsvp.SetLogger(logger.WithField("component", "rsvp.App")))
+	var app = rsvp.NewApp(counter, getNotificationSender(), rsvp.SetLogger(logger.WithField("component", "rsvp.App")))
 	if err = app.Run(); err != nil {
 		panic(err)
+	}
+}
+
+func getNotificationSender() notifications.Sender {
+	switch *notificationType {
+	case "telegram":
+		logger := logrus.New()
+		logger.SetFormatter(&logrus.TextFormatter{FullTimestamp: true})
+		logLevel, _ := logrus.ParseLevel(*logLevel) // safely ignore error should be handled by main
+		logger.SetLevel(logLevel)
+		sender, err := telegram.New(telegram.SetLogger(logger))
+		if err != nil {
+			panic(err)
+		}
+		return sender
+	default:
+		return stdout.NewSender()
 	}
 }
